5ce5e8f436486b1511246255e2cf09a274296e9b
Vedant36 <vedantneema@gmail.com>
2022-02-11 03:01:46 +0530

    Rewrote and optimized bar.sh
    - added clock synchronization for dwmblocks in xinitrc
    - bar.sh: switched to bash and shellchecked
    - added restart-bar for convenience

--- bin/scripts/bar.sh
+++ bin/scripts/bar.sh
@@ -1,30 +1,40 @@
-#!/bin/sh
-# script for dwmblocks
-# lines of changes in my dotfiles
-dot=" $(($(ad diff origin/main|wc -l) - $(ad diff origin/main ~/.local/share/zsh/.zsh_history|wc -l)))"
-# dot=$(ad diff origin/main | wc -l)
-uptime=" $(uptime -p)"
-# retrieve number of packages that can be upgraded that is cached using a
-# cronjob(currently disabled)
-packages=" $(if [ -s $LOG/packages.log ]; then wc -l <$LOG/packages.log;else printf 0;fi)"
-bat_state="$(upower -i "$(upower -e | grep BAT)" | grep -E 'state|time to|percentage' | sed 's/.*:\s*\(.*\)/\1/')"
-state="▲" && [ "$(grep discharging <<< $bat_state)" ] && state="▼"
-bat="${state} $(tail -n1 <<< $bat_state) $(tail -n2 <<< $bat_state | head -n1)"
-# bat="${state} $(acpi -b | grep -Po '[0-9]+(?=%)')% $(grep -v)"
-date=" $(date '+%a %F %H:%M')"
-music="$(mpc status | grep playing>/dev/null && mpc -f " %artist% - %title%" | sed -n 1p)"
+#!/usr/bin/env bash
+# Script that outputs the thing to put in dwmblocks(statusbar)
+# Rewritten by Vedant too late in the night(202202110249)
 
-modules=(
+# lines of changes in my dotfiles
+dot=" $(git --git-dir="$HOME"/.local/.git --work-tree="$HOME"/.local \
+    diff ~/.local/{bin,etc,lib,README.md} | wc -l)"
+
+uptime=" $(uptime | grep -Po 'up \K.*(?=,\s+\d+ user)' | xargs)"
+
+# the packages are updated in a cache everyday using anacrontab
+[ -s "$XDG_LOG_HOME"/packages.log ] && \
+    packages=" $(wc -l <"$XDG_LOG_HOME"/packages.log 2>/dev/null || printf 0)"
+
+# unreadable code to get battery percentage and time to full/empty
+[ -s "$XDG_CACHE_HOME"/BAT ] || upower -e | grep BAT > "$XDG_CACHE_HOME"/BAT
+bat_state="$(upower -i "$(cat "$XDG_CACHE_HOME"/BAT)" \
+    | grep -E 'state|time to|percentage' \
+    | sed 's/.*:\s*\(.*\)/\1/')"
+state="▲"; [[ "$bat_state" =~ discharging ]] && state="▼"
+bat="${state} $(tail -n2 <<< "$bat_state" | paste -sd ' ')"
+
+date=" $(date '+%H:%M %a %F')"
+
+music="$(mpc status | grep -q playing && \
+    mpc -f " %artist% - %title%" | sed -n 1p)"
+
+sections=(
 	"$dot"
-	# "$uptime"
+	"$uptime"
 	"$packages"
 	"$music"
 	"$bat"
 	"$date"
 )
 
-# echo -e "  ${dot}  ${uptime}  ${packages}  ${bat}  ${date} "
 echo -en " "
-for i in "${modules[@]}";do
+for i in "${sections[@]}";do
 	[ "$i" ] && echo -en "| $i "
 done
